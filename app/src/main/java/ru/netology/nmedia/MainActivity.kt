8f5648c305b696759c17725e818b38163c00e23f
Evgeny Belov <e.belov@inbox.ru>
2024-05-08 10:09:33 +0300

    find view by id
    Signed-off-by: Evgeny Belov <e.belov@inbox.ru>

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,16 +1,29 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+//        val binding=ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.ivLikes).setOnClickListener {
+            if (it !is ImageButton) {
+                return@setOnClickListener
+            }
+            it.setImageResource(R.drawable.clicked_likes)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
