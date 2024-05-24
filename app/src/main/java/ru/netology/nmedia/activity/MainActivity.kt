c011bad24add720fee1dd9cb975d05e46ddc7994
Evgeny Belov <e.belov@inbox.ru>
2024-05-24 22:50:47 +0300

    lection part1 all in one
    Signed-off-by: Evgeny Belov <e.belov@inbox.ru>

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.tools.Tools
 import ru.netology.nmedia.viemodel.PostViewModel
 
@@ -17,30 +18,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val tools = Tools()
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                tvAuthor.text = post.author
-                tvPublished.text = post.published
-                tvContent.text = post.content
-                tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
-                tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
-                tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
-                if (post.likedByMe) ibLikes.setImageResource(R.drawable.clicked_likes)
-                else ibLikes.setImageResource(
-                    R.drawable.likes
-                )
+        viewModel.data.observe(this) { posts ->
+            posts.map { post ->
+                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
+                    tvAuthor.text = post.author
+                    tvPublished.text = post.published
+                    tvContent.text = post.content
+                    tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
+                    tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
+                    tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
+                    ibLikes.setImageResource(if (post.likedByMe) R.drawable.clicked_likes else R.drawable.likes)
+                    ibLikes.setOnClickListener { viewModel.likeById(post.id) }
+                    ibShares.setOnClickListener { viewModel.shareById(post.id) }
+                }.root
+
             }
+//            with(binding) {
+//                tvAuthor.text = post.author
+//                tvPublished.text = post.published
+//                tvContent.text = post.content
+//                tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
+//                tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
+//                tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
+//                if (post.likedByMe) ibLikes.setImageResource(R.drawable.clicked_likes)
+//                else ibLikes.setImageResource(
+//                    R.drawable.likes
+//                )
+//            }
 
 
         }
-        with(binding) {
-            ibLikes.setOnClickListener {
-                viewModel.like()
-            }
-            ibShares.setOnClickListener {
-                viewModel.share()
-            }
-        }
+//        with(binding) {
+//            ibLikes.setOnClickListener {
+//                viewModel.like()
+//            }
+//            ibShares.setOnClickListener {
+//                viewModel.share()
+//            }
+//        }
 
 
     }
--- app/src/main/java/ru/netology/nmedia/viemodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viemodel/PostViewModel.kt
@@ -7,6 +7,6 @@ import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
-    fun like(id: Long) = repository.likeById(id)
-    fun share(id: Long) = repository.shareById(id)
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
 }
