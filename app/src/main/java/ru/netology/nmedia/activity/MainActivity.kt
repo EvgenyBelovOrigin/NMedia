28550a7085680e25ed3352fb7174f4c2414c6ddf
Evgeny Belov <e.belov@inbox.ru>
2024-05-26 22:20:22 +0300

    RecyclerView
    Signed-off-by: Evgeny Belov <e.belov@inbox.ru>

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,10 +3,8 @@ package ru.netology.nmedia.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.CardPostBinding
-import ru.netology.nmedia.tools.Tools
 import ru.netology.nmedia.viemodel.PostViewModel
 
 
@@ -16,49 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val tools = Tools()
+
         val viewModel: PostViewModel by viewModels()
+
+        val adapter = PostsAdapter({
+            viewModel.likeById(it.id)
+        }, {
+            viewModel.shareById(it.id)
+        })
+        binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            binding.container.removeAllViews()
-            posts.map { post ->
-                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
-                    tvAuthor.text = post.author
-                    tvPublished.text = post.published
-                    tvContent.text = post.content
-                    tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
-                    tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
-                    tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
-                    ibLikes.setImageResource(if (post.likedByMe) R.drawable.clicked_likes else R.drawable.likes)
-                    ibLikes.setOnClickListener {
-                        viewModel.likeById(post.id)
-                    }
-                    ibShares.setOnClickListener { viewModel.shareById(post.id) }
-                }.root
-
-            }
-//            with(binding) {
-//                tvAuthor.text = post.author
-//                tvPublished.text = post.published
-//                tvContent.text = post.content
-//                tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
-//                tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
-//                tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
-//                if (post.likedByMe) ibLikes.setImageResource(R.drawable.clicked_likes)
-//                else ibLikes.setImageResource(
-//                    R.drawable.likes
-//                )
-//            }
-
-
+            adapter.list = posts
         }
-//        with(binding) {
-//            ibLikes.setOnClickListener {
-//                viewModel.like()
-//            }
-//            ibShares.setOnClickListener {
-//                viewModel.share()
-//            }
-//        }
 
 
     }
---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,57 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.tools.Tools
+
+typealias OnLikeListener = (post: Post) -> Unit
+typealias OnShareListener = (post: Post) -> Unit
+
+class PostsAdapter(
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener
+) :
+    RecyclerView.Adapter<PostViewHolder>() {
+    var list = emptyList<Post>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener, onShareListener)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = list[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener,
+) : RecyclerView.ViewHolder(binding.root) {
+    val tools = Tools()
+    fun bind(post: Post) {
+        binding.apply {
+            tvAuthor.text = post.author
+            tvPublished.text = post.published
+            tvContent.text = post.content
+            tvLikesCount.text = tools.getShortCountTypeValue(post.likesCount)
+            tvSharesCount.text = tools.getShortCountTypeValue(post.sharesCount)
+            tvViewsCount.text = tools.getShortCountTypeValue(post.viewsCount)
+            ibLikes.setImageResource(if (post.likedByMe) R.drawable.clicked_likes else R.drawable.likes)
+            ibLikes.setOnClickListener { onLikeListener(post) }
+            ibShares.setOnClickListener { onShareListener(post) }
+        }
+    }
+}
