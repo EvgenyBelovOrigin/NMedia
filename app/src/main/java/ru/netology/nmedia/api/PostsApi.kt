df49ed1ffc1b39cb2e538d542a97d4f603120c00
Evgeny Belov <e.belov@inbox.ru>
2024-08-12 19:36:14 +0300

    with logging
    Signed-off-by: Evgeny Belov <e.belov@inbox.ru>

--- app/src/main/java/ru/netology/nmedia/api/PostsApi.kt
+++ app/src/main/java/ru/netology/nmedia/api/PostsApi.kt
@@ -1,5 +1,7 @@
 package ru.netology.nmedia.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,11 +10,26 @@ import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import ru.netology.nmedia.BuildConfig
 import ru.netology.nmedia.dto.Post
+import java.util.concurrent.TimeUnit
+
+private const val BASE_URL = "${BuildConfig.BASE_URL}/api/slow/"
+
+private val logging = HttpLoggingInterceptor().apply {
+    if (BuildConfig.DEBUG) {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
+private val okhttp = OkHttpClient.Builder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .addInterceptor(logging)
+    .build()
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(GsonConverterFactory.create())
-    .baseUrl("http://192.168.1.36:9999/api/slow/")
+    .baseUrl(BASE_URL)
+    .client(okhttp)
     .build()
 
 interface PostsApiService {
