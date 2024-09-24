10f42edd234c4281245194b1b3cd2d3d22fcab03
Evgeny <e.belov@inbox.ru>
2024-09-24 12:22:45 +0300

    Revert "wrong way"
    This reverts commit dca40ab0d3352a2fae588683aa7627db6f487bee.

--- app/src/main/java/ru/netology/nmedia/api/PostsApi.kt
+++ app/src/main/java/ru/netology/nmedia/api/PostsApi.kt
@@ -1,6 +1,5 @@
 package ru.netology.nmedia.api
 
-import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,12 +8,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Part
 import retrofit2.http.Path
 import ru.netology.nmedia.BuildConfig
-import ru.netology.nmedia.dto.Media
 import ru.netology.nmedia.dto.Post
 import java.util.concurrent.TimeUnit
 
@@ -54,10 +50,6 @@ interface PostsApiService {
 
     @DELETE("posts/{id}/likes")
     suspend fun disLikeById(@Path("id") id: Long): Response<Post>
-
-    @Multipart
-    @POST("media")
-    suspend fun upload(@Part file: MultipartBody.Part): Response<Media>
 
 
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -2,7 +2,6 @@ package ru.netology.nmedia.repository
 
 import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.model.PhotoModel
 
 interface PostRepository {
     val posts: Flow<List<Post>>
@@ -13,8 +12,6 @@ interface PostRepository {
     suspend fun getAll()
     suspend fun likeById(id: Long)
     suspend fun save(post: Post)
-    suspend fun saveWithAttachment(post: Post, photoModel: PhotoModel)
-
     suspend fun removeById(id: Long)
     suspend fun disLikeById(id: Long)
 }
